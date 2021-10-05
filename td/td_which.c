89aef75a4251b8d187b2288a931ee600c88b8478
Florian Vanhems <florian.vanhems@univ-lille.fr>
2021-10-05 11:48:50 +0200

    Fix td which

--- td/td_which.c
+++ td/td_which.c
@@ -1,9 +1,14 @@
 /* Commencer par le main */
 
+#include <stdbool.h>
+#include <limits.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+
+const char **get_path_dirs();
+bool which(const char *command, const char **path_directories);
 
 /* Contrat : 
  *  - EXIT_SUCCESS si les toutes commandes passées en
@@ -11,20 +16,24 @@
  *  - EXIT_FAILURE sinon */
 int main (int argc, char *argv[]) {
 	/* On s'assure que l'utilisateur a donné au moins une commande à chercher */
-	assert(argc > 1);
+	if (argc == 1) {
+		fprintf(stderr, "Error : expected command names to look for.\n"
+				"Usage : %s command1 [command2 ...]", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
 	/* On récupère les répertoires du PATH
 	 * ça nous permet de les `free()` à la fin de l'exécution */
-	char **path_directories = get_path_dirs();
+	const char **path_directories = get_path_dirs();
 
 	/* Flag mis à EXIT_FAILURE dès lors qu'une commande n'a pas été trouvée  */
 	int status = EXIT_SUCCESS;
 
 	/* Pour chaque commande passée en argument */
-	for (int command_n = 1; command_n < argc, command_n++) {
+	for (int command_n = 1; command_n < argc; command_n++) {
 		/* les chercher dans les différents répertoires du PATH */
 		/* si on ne trouve pas la commande dans tous les répertoires */
-		if (!which(argv[command_n]), path_directories) {
+		if (!which(argv[command_n], path_directories)) {
 			/* `which` échoue */
 			status = EXIT_FAILURE;
 		}
@@ -40,7 +49,7 @@ int main (int argc, char *argv[]) {
 /* Contrat :
  *   - retourne TRUE ou FALSE en fonction de si la commande a été trouvée ou non
  *   - Affiche le chemin de la commande si trouvée, affiche un message d'erreur sinon*/
-int which(const char *command, const char **path_directories) {
+bool which(const char *command, const char **path_directories) {
 	
 	/* déclare un tableau qui contiendra la concaténation
 	 * de la commande et du répertoire */
@@ -57,24 +66,27 @@ int which(const char *command, const char **path_directories) {
 		if (return_code == 0) {
 			/* on affiche le PATH de la commande */
 			printf("%s\n", command_path_buffer);
-			return TRUE;
+			return true;
 		}
 	}
 
 	/* Si la commande n'a été trouvée dans aucun des répertoires du PATH */
 	printf("%s : command not found\n", command);
-	return FALSE;
+	return false;
 }
 
-char **get_path_dirs() {
+const char **get_path_dirs() {
 	/* Récupère la chaine de la variable d'environnement PATH */
 	char *path = getenv("PATH");
 	/* On s'assure que la variable d'environnement existe */
-	assert(path != NULL);
+	if(path == NULL) {
+		perror("getenv");
+		exit(EXIT_FAILURE);	
+	}
 
 	unsigned dir_count = 1;
 	/* For each character of the path */
-	for (char *path_char_ptr = path; *path_char_ptr != NULL; path_char_ptr++) {
+	for (char *path_char_ptr = path; *path_char_ptr != '\0'; path_char_ptr++) {
 		/* Look for the delimiter ':' */
 		if(*path_char_ptr == ':') {
 			/* increase number of directories when found */
@@ -85,11 +97,14 @@ char **get_path_dirs() {
 	}
 
 	/* On crée un tableau du nombre de répertoires + 1 pour délimiter la fin du tableau */
-	char **path_dirs = malloc((dir_count + 1) * sizeof(char *));
-	assert(path_dirs != NULL);
+	const char **path_dirs = malloc((dir_count + 1) * sizeof(char *));
+	if(path_dirs == NULL){
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
 
 	/* On rempli le tableau avec les pointeurs de début de chaines des répertoires */
-	for(int i = 0; i < dir_count; i++) {
+	for(unsigned i = 0; i < dir_count; i++) {
 		path_dirs[i] = path;
 		path += strlen(path) + 1;
 	}
